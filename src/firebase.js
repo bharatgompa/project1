df5b00c9dc1c57d8a2e01dba478eaacab160cd65
bharatgompa <76870937+bharatgompa@users.noreply.github.com>
2021-05-07 21:40:41 +0530

    added files

---
+++ src/firebase.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
+
+const app = firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+})
+
+const firestore = app.firestore()
+export const batch = firestore.batch()
+export const database = {
+  folders: firestore.collection("folders"),
+  files: firestore.collection("files"),
+  formatDoc: doc => {
+    return { id: doc.id, ...doc.data() }
+  },
+  getCurrentTimestamp: firebase.firestore.FieldValue.serverTimestamp,
+}
+export const storage = app.storage()
+export const auth = app.auth()
+export default app
